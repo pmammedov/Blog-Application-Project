e3e968442ef44463685575631cbc54eac0f2b198
pmammedov <pmammedov@gmail.com>
2024-07-11 19:21:35 +0200

    feat: Add new pages for About, Login, NewBlog, Profile, Register, Dashboard, and UpdateBlog
    This commit adds new pages for About, Login, NewBlog, Profile, Register, Dashboard, and UpdateBlog. Each page is a functional component that renders a simple div with the corresponding page name. These pages will serve as the foundation for the frontend of the blog application project.

---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from '../components/Navbar';
+import About from '../pages/About';
+import Dashboard from '../pages/Dashboard';
+import Login from '../pages/Login';
+import NewBlog from '../pages/NewBlog';
+import Profile from '../pages/Profile';
+import Register from '../pages/Register';
+import UpdateBlog from '../pages/UpdateBlog';
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/newblog' element={<NewBlog />} />
+                <Route path='/update' element={<UpdateBlog />} />
+                <Route path='/profile' element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRouter
